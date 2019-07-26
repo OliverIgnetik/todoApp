6d32546560bab9fa62145194aec41eb8b6bb5acd
Oliver Ignetik <olli@ozemail.com.au>
2019-07-26 15:00:45 +1000

    Created todo-list and formatter app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const todos = ['Learn React', 'Learn Redux', 'Learn React Native']
+
+class App extends Component {
+  render() {
+    return React.createElement(
+      'div',
+      {
+        className: 'app',
+      },
+      React.createElement(
+        'div',
+        { className: 'todo-container' },
+        // in JSX placing {} around the output of an arrow function 
+        // will change the output in the HTML
+        todos.map((todo, index) => 
+          React.createElement(
+            'div',
+            {
+              className: 'todo-item-row',
+              key: index,
+            },
+            todo,
+          )
+        ),
+      ),
+    )
+  }
 }
 
-export default App;
+export default App
