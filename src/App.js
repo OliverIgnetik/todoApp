c8a76ac6487b81c211d9fe183e27427c79acfaea
Oliver Ignetik <olli@ozemail.com.au>
2019-07-26 17:35:37 +1000

    Included semantic-react-ui and semantic-ui-css added state to component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
 import React, { Component } from 'react'
+import { Table, Checkbox, Button, Input } from 'semantic-ui-react'
 import './App.css'
 
-const todos = [
-  'Learn React',
-  'Learn Redux',
-  'Learn React Native',
-  'make a web app',
-]
+// const todos = [
+//   'Learn React',
+//   'Learn Redux',
+//   'Learn React Native',
+//   'Make a web app',
+// ]
 
-// .todo-item-row {
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center
-// }
-
-// destructure on props children is always part of props
-const TodoItem = ({ children }) => (
-  <div
-    style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-    }}
-    className='todo-item-row'
-  >
-    {children})
-  </div>
+// construct TodoItem
+const TodoItem = props => (
+  <Table.Row>
+    <Table.Cell>
+      <Checkbox />
+    </Table.Cell>
+    <Table.Cell>
+      {props.children}
+      <Button color='red' icon='trash' floated='right' compact size='small' />
+    </Table.Cell>
+  </Table.Row>
 )
 
 class App extends Component {
+  // set state in a component
+  state = {
+    todos: [
+      'Learn React',
+      'Learn Redux',
+      'Learn React Native',
+      'Make a web app',
+    ],
+  }
   render() {
     return (
       <div className='app'>
         <div className='todo-container'>
-          <input id='new-todo' />
-          <label htmlFor='new-todo' />
-          {todos.map((todo, index) => (
-            <TodoItem key={index}>{todo}</TodoItem>
-          ))}
+          <Input
+            className='new-todo'
+            action='Add Item'
+            placeholder='What do you need to do?'
+          />
+          <Table>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>
+                  <Checkbox />
+                </Table.HeaderCell>
+                <Table.HeaderCell>Toggle all items</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {this.state.todos.map((todo, index) => (
+                <TodoItem key={index}>{todo}</TodoItem>
+              ))}
+            </Table.Body>
+          </Table>
         </div>
       </div>
     )
