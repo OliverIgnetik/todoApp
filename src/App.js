dfccf70291fae3c9961d3c12ea84b45dadb6ed94
Oliver Ignetik <olli@ozemail.com.au>
2019-07-26 16:49:57 +1000

    Inline styles in React

--- src/App.js
+++ src/App.js
@@ -8,18 +8,35 @@ const todos = [
   'make a web app',
 ]
 
-let token = true;
+// .todo-item-row {
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center
+// }
+
+// destructure on props children is always part of props
+const TodoItem = ({ children }) => (
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    }}
+    className='todo-item-row'
+  >
+    {children})
+  </div>
+)
+
 class App extends Component {
   render() {
     return (
       <div className='app'>
         <div className='todo-container'>
           <input id='new-todo' />
-          <label htmlFor="new-todo"></label>
+          <label htmlFor='new-todo' />
           {todos.map((todo, index) => (
-            <div key={index} className='todo-item-row'>
-              {todo}
-            </div>
+            <TodoItem key={index}>{todo}</TodoItem>
           ))}
         </div>
       </div>
